f35cc2cef1067dccbb0f2df5873ec8c94ee7d7cb
Olena Zamyslova <olenazamyslova@gmail.com>
2023-04-03 13:31:53 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -5,67 +5,57 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
 const App = () => {
-const [contacts, setContacts] = useState([
-{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]);
-const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  });
+  const [filter, setFilter] = useState('');
 
-useEffect(() => {
-const savedContacts = localStorage.getItem('contacts');
-if (savedContacts) {
-setContacts(JSON.parse(savedContacts));
-}
-}, []);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-useEffect(() => {
-localStorage.setItem('contacts', JSON.stringify(contacts));
-}, [contacts]);
+  const repeatControl = (data) => {
+    const isExist = contacts.find((contact) => contact.name === data.name);
+    if (isExist) {
+      alert('Контакт вже є у телефонній книзі!!!');
+      return;
+    }
+    const newContact = {
+      id: uuidv4(),
+      name: data.name,
+      number: data.number,
+    };
+    setContacts(prevState => [...prevState, newContact]);
+  };
 
-const repeatControl = (data) => {
-const isExist = contacts.find((contact) => contact.name === data.name);
-if (isExist) {
-alert('Контакт вже є у телефонній книзі!!!');
-return;
-}
-  const newContact = {
-  id: uuidv4(),
-  name: data.name,
-  number: data.number,
-};
-  setContacts([...contacts, newContact]);
+  const deleteContactFromContactList = (idContact) => {
+    setContacts(prevState => {
+      const updatedContacts = prevState.filter((contact) => contact.id !== idContact);
+      return updatedContacts;
+    });
+  };
 
+  const filterArr = () => {
+    return contacts.filter((cur) => cur.name.toUpperCase().includes(filter.toUpperCase()));
+  };
 
-};
+  const filteredContacts = filterArr();
 
-const elementDelete = (arr, idContact) => {
-return arr.filter((elem) => elem.id !== idContact);
-};
-
-const deleteContactFromContactList = (idContact) => {
-const updatedContacts = elementDelete(contacts, idContact);
-setContacts(updatedContacts);
-};
-
-const filterArr = () => {
-return contacts.filter((cur) =>
-cur.name.toUpperCase().includes(filter.toUpperCase())
-);
-};
-
-const filteredContacts = filterArr();
-
-return (
-<div className="App">
-<h1>Phonebook</h1>
-<ContactForm onSubmitData={repeatControl} />
-<h1>Contacts</h1>
-<Filter setFilterToState={setFilter} />
-<ContactList contacts={filteredContacts} del={deleteContactFromContactList} />
-</div>
-);
+  return (
+    <div className="App">
+      <h1>Phonebook</h1>
+      <ContactForm onSubmitData={repeatControl} />
+      <h1>Contacts</h1>
+      <Filter setFilterToState={setFilter} />
+      <ContactList contacts={filteredContacts} del={deleteContactFromContactList} />
+    </div>
+  );
 };
 
 export default App;
